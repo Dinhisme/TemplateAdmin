9ba575608297ed260875045da730af00e3f66737
Dinhisme <phungquocvinh15112003@gmail.com>
2023-08-22 23:40:55 +0700

    Upload template

---
+++ js/index.js
@@ -0,0 +1,62 @@
+const sideLinks = document.querySelectorAll(
+  ".sidebarD .side-menu li a:not(.logout)"
+);
+
+sideLinks.forEach((item) => {
+  const li = item.parentElement;
+  item.addEventListener("click", () => {
+    sideLinks.forEach((i) => {
+      i.parentElement.classList.remove("active");
+    });
+    li.classList.add("active");
+  });
+});
+
+const menuBar = document.querySelector(".content nav .bx.bx-menu");
+const sideBar = document.querySelector(".sidebarD");
+
+menuBar.addEventListener("click", () => {
+  sideBar.classList.toggle("closeD");
+});
+
+const searchBtn = document.querySelector(
+  ".content nav form .form-input button"
+);
+const searchBtnIcon = document.querySelector(
+  ".content nav form .form-input button .bx"
+);
+const searchForm = document.querySelector(".content nav form");
+
+searchBtn.addEventListener("click", function (e) {
+  if (window.innerWidth < 576) {
+    e.preventDefault;
+    searchForm.classList.toggle("show");
+    if (searchForm.classList.contains("show")) {
+      searchBtnIcon.classList.replace("bx-search", "bx-x");
+    } else {
+      searchBtnIcon.classList.replace("bx-x", "bx-search");
+    }
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth < 1000) {
+    sideBar.classList.add("closeD");
+  } else {
+    sideBar.classList.remove("closeD");
+  }
+  if (window.innerWidth > 576) {
+    searchBtnIcon.classList.replace("bx-x", "bx-search");
+    searchForm.classList.remove("show");
+  }
+});
+
+const toggler = document.getElementById("theme-toggle");
+
+toggler.addEventListener("change", function () {
+  if (this.checked) {
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+  }
+});
